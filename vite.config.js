baa4ca6e7a9086c884a04eb5d84e08fe5f360abc
janry <janryseran069407@gmail.com>
2025-10-11 10:33:15 +0800

    fixed vite with addition codes and app.css to apply

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '', // <-- keep paths relative so /hr-attendance works
+    build: {
+        assetsDir: 'build/assets', // ensure all assets go under /build/assets
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
